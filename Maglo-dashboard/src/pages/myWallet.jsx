11872c58db90e37b2aff51e5eadbe50e9d06e725
Dina Amr <58145677+dinaamrr@users.noreply.github.com>
2022-11-17 05:24:37 +0200

    Update myWallet.jsx

--- Maglo-dashboard/src/pages/myWallet.jsx
+++ Maglo-dashboard/src/pages/myWallet.jsx
@@ -1,16 +1,79 @@
 import React from 'react'
 import TopBar from '../components/top-nav/top-nav'
 import card from '../assets/images/Capture.PNG'
+import balance from '../assets/images/card.PNG'
 import '../styles/myWallet.css'
 import {AiOutlineArrowUp} from 'react-icons/ai'
 import { Link } from 'react-router-dom';
 import {AiOutlineArrowDown} from 'react-icons/ai'
+import {BsSearch} from 'react-icons/bs';
+import {AiOutlinePlus} from 'react-icons/ai';
+const  data = [
+    {
+      "img":require("../assets/images/payoneer.png") ,
+      "title": "Payoneer",
+      "date" : "20 Apr 2022, 06:55 PM",
+      "paymentValue":"+$4800.24",
+      "id" : 1
+    },
+    
+    {
+      "img":require("../assets/images/Remitly.png") ,
+      "title": "Remitly",
+      "date" : "18 Apr 2022, 08:58 PM",
+      "paymentValue":"+$4800.24",
+      "id" : 2
+    },
+    
+    {
+      "img":require("../assets/images/wise.png") ,
+      "title": "Wise",
+      "date" : "15 Apr 2022, 02:55 AM",
+      "paymentValue":"+$4800.24",
+      "id" : 3
+    },
+    {
+      "img":require("../assets/images/paypal.png") ,
+      "title": "Paypal",
+      "date" : "14 Apr 2022, 07:40 PM",
+      "paymentValue":"+$4800.24",
+      "id" : 4
+    }
+    
+    ]
+    
+    
+    
+    // Up coming payments 
+     
+     
+     
+     
+    export const  upComingPayments = [
+      {
+        "img":require("../assets/images/facebook.png") ,
+        "title": "Facebooks Ads",
+        "date" : "20 Apr 2022, 06:55 PM",
+        "paymentValue":"$4800",
+        "id" : 1
+      },
+      
+      {
+        "img":require("../assets/images/linkedinLogo.png") ,
+        "title": "LinkedIn Ads",
+        "date" : "18 Apr 2022, 08:58 PM",
+        "paymentValue":"$200.50",
+        "id" : 2
+      },
+      
+    
+      ]
 const MyWallet = () => {
   return (
     <main>
     <div className='dashboard-container'>
     <TopBar title='My Wallets' />
-    <div className='column column-two'>
+    <div className='column column-twoo'>
     <div className='dashboard-spending-cards'>
     <img src={card} alt=''/>
       
@@ -22,6 +85,11 @@ const MyWallet = () => {
   <h3>Working Capital</h3>
   </div>
  
+  <div>
+  <img src={balance}alt=''/>
+  </div>
+  
+  <button className='btn'><AiOutlinePlus/> Add New Card</button>
   </div>
 
 
@@ -34,102 +102,49 @@ const MyWallet = () => {
 
 
 
-<div className='column column-one'>
+<div className='column column-onee'>
           <div className='recurring-expenses-container'>
             <div className='expenses-header'>
-              <h3 className='expenses-title'>My Payments</h3>
-              <Link to='/myWallet' className='expenses-view-all'>
-                ...
+              <h3 className='expenses-title'>My Payments <br/><br/><li>All Payments</li>
+              <li>&nbsp; &nbsp;Regular Payments</li> <hr/> </h3>
+           
+              <div className='expenses-view-all'>
+              <BsSearch/> Search
                
              
-              </Link>
+              </div>
              
                
             </div>
-            <div class="container">
-            <div class="row">
-                
-                  <div class="articles card">
-                  
-                    
-                    <div className="card-body ">
-                      <div className="item d-flex align-items-center">
-                        <div class="image"><img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="..." class="img-fluid rounded-circle"/></div>
-                        <div className="text "><a href="#">
-                            <h3 class="h5">Lorem Ipsum Dolor</h3></a><small>Posted on 5th June 2017 by Aria Smith.   </small></div>
-                      </div>
-                      <div class="item d-flex align-items-center">
-                        <div class="image"><img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="..." class="img-fluid rounded-circle"/></div>
-                        <div class="text"><a href="#">
-                            <h3 class="h5">Lorem Ipsum Dolor</h3></a><small>Posted on 5th June 2017 by Frank Williams.   </small></div>
-                      </div>
-                      <div class="item d-flex align-items-center">
-                        <div class="image"><img src="https://bootdey.com/img/Content/avatar/avatar4.png" alt="..." class="img-fluid rounded-circle"/></div>
-                        <div class="text"><a href="#">
-                            <h3 class="h5">Lorem Ipsum Dolor</h3></a><small>Posted on 5th June 2017 by Ashley Wood.   </small></div>
-                      </div>
-                      <div class="item d-flex align-items-center">
-                        <div class="image"><img src="https://bootdey.com/img/Content/avatar/avatar5.png" alt="..." class="img-fluid rounded-circle"/></div>
-                        <div class="text"><a href="#">
-                            <h3 class="h5">Lorem Ipsum Dolor</h3></a><small>Posted on 5th June 2017 by Jason Doe.   </small></div>
-                      </div>
-                      <div class="item d-flex align-items-center">
-                        <div class="image"><img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="..." class="img-fluid rounded-circle"/></div>
-                        <div class="text"><a href="#">
-                            <h3 class="h5">Lorem Ipsum Dolor</h3></a><small>Posted on 5th June 2017 by Sam Martinez.   </small></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                </div>
+            <small className='sss'>Today</small>
+            <div>
+            {data.map(user =>
+           
+               <li className='mw'>< img src={user.img} width="40px"alt={user.title} />{user.title} <br/><small>{user.date}</small> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;{user.paymentValue}</li>
+              
+             
+              )}
+          </div>
+           
           </div>
           <div className='recurring-expenses-container'>
           <div className='expenses-header'>
             <h3 className='expenses-title'>Upcoming Payments</h3>
-            <Link to='/transactions' className='expenses-view-all'>
-            View All{' '}
-             
-           
-            </Link>
-           
-             
-          </div>
-          <div class="container">
-    <div class="row">
-        
-          <div class="articles card">
-          
-            
-            <div className="card-body ">
-              <div className="item d-flex align-items-center">
-                <div class="image"><img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="..." class="img-fluid rounded-circle"/></div>
-                <div className="text "><a href="#">
-                    <h3 class="h5">Lorem Ipsum Dolor</h3></a><small>Posted on 5th June 2017 by Aria Smith.   </small></div>
-              </div>
-              <div class="item d-flex align-items-center">
-                <div class="image"><img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt="..." class="img-fluid rounded-circle"/></div>
-                <div class="text"><a href="#">
-                    <h3 class="h5">Lorem Ipsum Dolor</h3></a><small>Posted on 5th June 2017 by Frank Williams.   </small></div>
-              </div>
-              <div class="item d-flex align-items-center">
-                <div class="image"><img src="https://bootdey.com/img/Content/avatar/avatar4.png" alt="..." class="img-fluid rounded-circle"/></div>
-                <div class="text"><a href="#">
-                    <h3 class="h5">Lorem Ipsum Dolor</h3></a><small>Posted on 5th June 2017 by Ashley Wood.   </small></div>
-              </div>
-              <div class="item d-flex align-items-center">
-                <div class="image"><img src="https://bootdey.com/img/Content/avatar/avatar5.png" alt="..." class="img-fluid rounded-circle"/></div>
-                <div class="text"><a href="#">
-                    <h3 class="h5">Lorem Ipsum Dolor</h3></a><small>Posted on 5th June 2017 by Jason Doe.   </small></div>
-              </div>
-              <div class="item d-flex align-items-center">
-                <div class="image"><img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt="..." class="img-fluid rounded-circle"/></div>
-                <div class="text"><a href="#">
-                    <h3 class="h5">Lorem Ipsum Dolor</h3></a><small>Posted on 5th June 2017 by Sam Martinez.   </small></div>
-              </div>
+                
             </div>
+            <small className='sss'>Next Month</small>
+            <div>
+            {upComingPayments.map(user =>
+              
+               <li className='mw'>< img src={user.img} width="40px"alt={user.title} />{user.title} <br/><small>{user.date}</small> &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;  {user.paymentValue}</li>
+              
+             
+              )}
           </div>
-        </div>
-        </div>
+             
+        
+         
+      
         </div>
 
 </div>
