9983631d3d609ca02e39aa90b9f3061b275daf5d
Dina Amr <58145677+dinaamrr@users.noreply.github.com>
2022-11-17 20:23:32 +0200

    Update transactions.jsx

--- Maglo-dashboard/src/pages/transactions.jsx
+++ Maglo-dashboard/src/pages/transactions.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import TopBar from '../components/top-nav/top-nav'
 
 import '../styles/transactions.css'
@@ -124,7 +124,10 @@ const data = [
   
 ];
 const Transactions = () => {
+  const [filteredList, setFilteredList] = useState("");
+
     return (
+      <main>
         <div className='tconatiner'>
         <TopBar title='Transactions' />
         <div className="inner-form">
@@ -135,7 +138,7 @@ const Transactions = () => {
                   <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
                 </svg>
               </button>
-              <input className='search' type="search" placeholder="Search" />
+              <input className='search' type="search" placeholder="Search" onChange={(e)=> setFilteredList(e.target.value)} />
             </div>
           </div>
         <table className="table">
@@ -150,7 +153,12 @@ const Transactions = () => {
             </tr>
         </thead>
         <tbody>
-            {data.map(user =>
+        {data.filter((datas)=>
+
+          datas.name.toLowerCase().includes(filteredList)
+          
+        
+            ).map((user =>
                 
                 <tr key={user.id}>
                     <td><img src={user.img}  width='40px'alt=''/>{user.name}<br/><small>{user.small}</small> </td>
@@ -164,10 +172,11 @@ const Transactions = () => {
                    
                 </tr>
                
-            )}
+    ))}
         </tbody>
     </table>
     </div>
+    </main>
   );
 }
  
